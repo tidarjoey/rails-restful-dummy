a164e5798a7d78e449c58faf3eed0e98ec6703ba
Tidar <tidarharyo@rocketmail.com>
2019-02-07 01:19:43 +0530

    develop create feature

--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -7,7 +7,19 @@ class ComputersController < ApplicationController
   end
 
   def new
+    @computers = Computer.new
   end
+
+  def create
+    computer = Computer.create(computer_params)
+
+    redirect_to commputers_path
+  end
+
+  private
+    def computer_param
+      params.require(:computer).permit(:name, :description)
+    end
 
   def edit
   end
